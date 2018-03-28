c294f00f3f012d832383cc848a0202ffeb194eb5
SERGI-PORTATIL\Sergi Regi <sergiregi88@gmail.com>
2018-03-28 11:30:25 +0200

    added the daisy behabiur of random go to point and when bouquet apprears the hearts apears

---
+++ BTs_STU/Assets/BTs/DAISYs/Daisy_Blackboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Daisy_Blackboard : MonoBehaviour {
+    public GameObject[] waypoints;
+
+	// Use this for initialization
+	public GameObject GetRandomWaypoint()
+    {
+        return waypoints[Random.Range(0, waypoints.Length - 1)];
+    }
+}
